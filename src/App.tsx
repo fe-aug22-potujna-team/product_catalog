8ad85122cd0cd280b798a7e1db7f351f72bef5ba
Roman Starovoit <rstarovoit@gmail.com>
2022-11-30 15:16:33 +0200

    empty components created 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-
 import { Header } from './components/Header'
 import { PhoneCard } from './components/PhoneCard';
 import { Footer } from './components/Footer'
