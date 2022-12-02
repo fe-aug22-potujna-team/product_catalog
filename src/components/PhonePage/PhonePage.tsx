cf284bd6795eb035be5287321e25e9a924d318c1
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2022-12-02 23:06:07 +0200

    test 1

--- src/components/PhonePage/PhonePage.tsx
+++ src/components/PhonePage/PhonePage.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PhonePage.scss';
+import { PhoneCardList } from '../PhoneCardList/PhoneCardList';
+import { Pagination } from '../Pagination/Pagination';
 
 export const PhonePage: React.FC = () => {
+  const phonesAmount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+  const [currentPage, setCurrentPage] = useState(1)
+  const [cardPerPage, setCardPerPage] = useState(4);
+
+  const lastCardIndex = currentPage * cardPerPage;
+  const firstCardIndex = lastCardIndex - cardPerPage;
+  const currentCards = phonesAmount.slice(firstCardIndex, lastCardIndex);
+
   return (
-    <>
-      <div>Phones</div>
-    </>
+    <div className='PhonePage'>
+      <h1 className='PhonePage__title'>Mobile phones</h1>
+
+      <div>
+          <select onChange={(e) => setCardPerPage(+e.target.value)} name="" id="">
+            <option value="4">4</option>
+            <option value="8">8</option>
+            <option value="16">16</option>
+          </select>
+      </div>
+
+      <PhoneCardList phonesAmount={currentCards}/>
+      <Pagination 
+        cardPerPage={cardPerPage} 
+        totalCards={phonesAmount.length} 
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
+    </div>
   );
 };
