415d170b9fd73aa03bb6655a79c24bfb52f8495f
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2022-12-04 14:38:34 +0200

    edd enum

--- src/components/PhonePage/PhonePage.tsx
+++ src/components/PhonePage/PhonePage.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './PhonePage.scss';
 import { PhoneCardList } from '../PhoneCardList/PhoneCardList';
 import { Pagination } from '../Pagination/Pagination';
+import house from '../../images/icons/house.svg'
+import arrow from '../../images/icons/arrow.svg'
+import { SortPhones } from '../../Enums/Enums'
 
 export const PhonePage: React.FC = () => {
   const phonesAmount = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
@@ -14,14 +17,34 @@ export const PhonePage: React.FC = () => {
 
   return (
     <div className='PhonePage'>
+      <div className='PhonePage__rout Rout'>
+        <img  className='PhonePage__margin Rout__img' src={house} alt="img" />
+        <img  className='Rout__img' src={arrow} alt="img" />
+        <p className='Rout__text'>Phones</p>
+      </div>
+
       <h1 className='PhonePage__title'>Mobile phones</h1>
 
-      <div>
-          <select onChange={(e) => setCardPerPage(+e.target.value)} name="" id="">
-            <option value="4">4</option>
-            <option value="8">8</option>
-            <option value="16">16</option>
-          </select>
+      <h1 className='PhonePage__models'>5 models</h1>
+      
+      <div className='PhonePage__setting Setting'>
+          <div className='PhonePage__margin'>
+            <p className='Setting__title'>Sort by</p>
+            <select className='Setting__select Setting__select-sort'>
+              <option value={SortPhones.Newest}>Newest</option>
+              <option value={SortPhones.Alphabetically}>Alphabetically</option>
+              <option value={SortPhones.Cheapest}>Cheapest</option>
+            </select>
+          </div>
+
+          <div>
+            <p className='Setting__title'>Items on page</p>
+            <select className='Setting__select' onChange={(e) => setCardPerPage(+e.target.value)}>
+              <option value="4">4</option>
+              <option value="8">8</option>
+              <option value="16">16</option>
+            </select>
+          </div>
       </div>
 
       <PhoneCardList phonesAmount={currentCards}/>
