3c9a27e477c4d77562ef8cd6c8ad0d794ed13a2b
korniienkokarina <k.tashlytska@gmail.com>
2022-12-09 19:07:09 +0200

    footer fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer: React.FC = () => {
         </ul>
         <div className='footer__backtop'>
           <p className='footer__backtop-text'>Back to top</p>
-          <a href='/product_catalog/'>
+          <a href='/product_catalog/' className='back-link'>
             <img src={back} alt='back top' className='footer__backtop-icon'></img>
           </a>
         </div>
